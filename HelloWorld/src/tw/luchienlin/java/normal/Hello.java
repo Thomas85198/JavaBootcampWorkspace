19dd38c748a458c75d07df2da9ab97f0c37a2e50
Chienlin <luchienlin1994@gmail.com>
2020-01-28 23:48:30 +0800

    Hello Java
    非常簡單的Java內容變數...

---
+++ HelloWorld/src/tw/luchienlin/java/normal/Hello.java
@@ -0,0 +1,17 @@
+package tw.luchienlin.java.normal;
+public class Hello {
+	
+	public static void main(String[] args) {
+		// public 是類別存取的修飾字，可以准許我們存取Java code在裡面
+		// void 即代表此方法不會回傳任何東西
+		
+		System.out.println("Hello Thomas");
+	
+		int myFirstNumber = (10 + 5) + (2 * 10);
+		// Declaration Statement 宣告
+		// Expression 表達單值的構造
+		
+		System.out.println(myFirstNumber);
+		// String Literal 雙引號刮著的字串，不像變數可以改變
+	}
+}
