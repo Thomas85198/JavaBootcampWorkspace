758364b323f1dec35c384823ae8f8bb2f131d1a4
Chienlin <luchienlin1994@gmail.com>
2020-02-01 17:10:44 +0800

    switch case

---
+++ Switch/src/tw/luchienlin/java/loop/Exercise.java
@@ -0,0 +1,26 @@
+package tw.luchienlin.java.loop;
+
+public class Exercise {
+
+	public static void main(String[] args) {
+		
+		char var1 = 'A';
+		
+		switch(var1) {
+		case 'A':
+			System.out.println("The char is A");
+			break;
+		case 'B':
+			System.out.println("The char is B");
+			break;
+		case 'C':
+			System.out.println("The char is C");
+			break;
+		default:
+			System.out.println("NOT FOUND");
+			break;
+		}
+			
+	}
+
+}
